b2051ef12de19e1ccf5dd614bd696a011cbdba09
Kpokodul <51588090+Kpokodul@users.noreply.github.com>
2020-10-10 07:37:18 +0300

    Структура файлов

---
+++ code/v0.1/Pascal/2to10.pas
@@ -0,0 +1,14 @@
+﻿var c, b:string;
+    a:integer;
+begin
+  readln(a);
+  c := '';
+  while a > 0 do
+  begin
+    c := c + inttostr(a mod 2);
+    a := a div 2;
+  end;
+  for var i := length(c) downto 1 do
+    b:= b+c[i];
+  writeln(b);
+end.
